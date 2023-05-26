685d5821eea81f9d2d7e916437a8032dd6cc1c26
wijay58 <supun.wijemanna58@gmail.com>
2023-05-26 15:57:24 +0530

    new user fields

--- models/user.js
+++ models/user.js
@@ -2,39 +2,48 @@ var mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 let UserSchema = new Schema({
-    email: {
-        type: mongoose.Schema.Types.String
-    },
-    phone: {
-        type: mongoose.Schema.Types.String
-    },
-    password: {
-        type: mongoose.Schema.Types.String
-    },
-    imageUrl: {
-      type: mongoose.Schema.Types.String
-    },
-    firstname: {
-        type: mongoose.Schema.Types.String
-    },
-    lastname: {
-        type: mongoose.Schema.Types.String
-    },
-    premium: {
-        type: mongoose.Schema.Types.Boolean,
-        default: false
-    },
-    businessDescription: {
-        type: mongoose.Schema.Types.String,
-        default: 'Click to edit your business description.'
-    },
-    user_type: {
-        type: mongoose.Schema.Types.Number,
-        default: 0
-        // 0=employee 1=admin
-    },
+  email: {
+    type: mongoose.Schema.Types.String
+  },
+  phone: {
+    type: mongoose.Schema.Types.String
+  },
+  address: {
+    type: mongoose.Schema.Types.String
+  },
+  country: {
+    type: mongoose.Schema.Types.String
+  },
+  bio: {
+    type: mongoose.Schema.Types.String
+  },
+  password: {
+    type: mongoose.Schema.Types.String
+  },
+  imageUrl: {
+    type: mongoose.Schema.Types.String
+  },
+  firstname: {
+    type: mongoose.Schema.Types.String
+  },
+  lastname: {
+    type: mongoose.Schema.Types.String
+  },
+  premium: {
+    type: mongoose.Schema.Types.Boolean,
+    default: false
+  },
+  businessDescription: {
+    type: mongoose.Schema.Types.String,
+    default: 'Click to edit your business description.'
+  },
+  user_type: {
+    type: mongoose.Schema.Types.Number,
+    default: 0
+    // 0=employee 1=admin
+  },
 }, {
-    timestamps: true
+  timestamps: true
 })
 
 module.exports = mongoose.model('User', UserSchema);
